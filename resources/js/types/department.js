02d4bb219c3f037366c7b364ae32324dba822741
Merlyn8989 <merlynrajo@gmail.com>
2026-04-06 15:00:28 -0600

    otros archivos transformados

---
+++ resources/js/types/department.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef {Object} DepartmentBasic
+ * @property {number} id
+ * @property {string} name
+ */
+
+/**
+ * @typedef {DepartmentBasic} Department
+ * @property {string|null} description
+ * @property {string|null} email_department
+ * @property {number|null} area_id
+ * @property {string|null} deleted_at
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+
